16718d0db3973b70a21e8c4eed9050c335153d55
cwingate <chris.wingate@developertown.com>
2013-11-06 22:10:02 -0500

    Update wicked_pdf.rb
    Add support for 0.10.0+ toc back links

--- lib/wicked_pdf.rb
+++ lib/wicked_pdf.rb
@@ -241,7 +241,8 @@ class WickedPdf
                                     :disable_javascript,
                                     :print_media_type,
                                     :disable_smart_shrinking,
-                                    :no_background], "", :boolean)
+                                    :no_background,
+                                    :enable_toc_back_links], "", :boolean)
       end
     end
 
